04fa8f67aa6a4fdbf34bef06fdc9d9514d0e38c5
Cosmin Popa <cosmin7popa@gmail.com>
2024-11-21 21:40:34 +0200

    Fix sprite rescale & extract to utils
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,19 @@ class Direction(Enum):
     RIGHT = 2
     UP = 3
     DOWN = 4
+
+def scale_surface(surface, scale):
+    size_x, size_y = surface.get_size()
+
+    original_ratio = size_x / size_y
+    new_ratio = scale[0] / scale[1]
+
+    if new_ratio > original_ratio:
+        scale_y = scale[1]
+        scale_x = scale_y * original_ratio
+    else:
+        scale_x = scale[0]
+        scale_y = scale_x / original_ratio
+    
+    return pygame.transform.scale(surface, (scale_x, scale_y))
+
