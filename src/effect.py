bac589d230dec2137f00f5b3046da53ac50ff19d
Cosmin Popa <cosmin7popa@gmail.com>
2024-12-07 19:48:56 +0200

    Remove unnecessary effect file
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

--- src/effect.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from BloomEffect import bloom_effect24, bloom_effect32
-import pygame
-
-class Effect(ABC):
-    @abstractmethod
-    def apply(self, surface):
-        raise NotImplementedError("Apply method must be implemented by subclasses")
