94db760e48fef4514f7d381ff9c17446132ea449
denisa.ciobanu1208 <denisa.ciobanu1208@stud.acs.upb.ro>
2024-12-09 19:33:13 +0200

    level3 map
    Signed-off-by: denisa.ciobanu1208 <denisa.ciobanu1208@stud.acs.upb.ro>

---
+++ assets/winter-tilemap/winter-tileset.tsx
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="winter" tilewidth="96" tileheight="96" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="tiles/1.png" width="96" height="96"/>
+ </tile>
+ <tile id="1">
+  <image source="tiles/2.png" width="96" height="96"/>
+ </tile>
+ <tile id="2">
+  <image source="tiles/3.png" width="96" height="96"/>
+ </tile>
+ <tile id="3">
+  <image source="tiles/4.png" width="96" height="96"/>
+ </tile>
+ <tile id="4">
+  <image source="tiles/5.png" width="96" height="96"/>
+ </tile>
+ <tile id="5">
+  <image source="tiles/6.png" width="96" height="96"/>
+ </tile>
+ <tile id="6">
+  <image source="tiles/7.png" width="96" height="96"/>
+ </tile>
+ <tile id="7">
+  <image source="tiles/8.png" width="96" height="96"/>
+ </tile>
+ <tile id="8">
+  <image source="tiles/9.png" width="96" height="96"/>
+ </tile>
+ <tile id="9">
+  <image source="tiles/10.png" width="96" height="96"/>
+ </tile>
+ <tile id="10">
+  <image source="tiles/11.png" width="96" height="96"/>
+ </tile>
+ <tile id="11">
+  <image source="tiles/12.png" width="96" height="96"/>
+ </tile>
+ <tile id="12">
+  <image source="tiles/13.png" width="96" height="96"/>
+ </tile>
+ <tile id="13">
+  <image source="tiles/14.png" width="96" height="70"/>
+ </tile>
+ <tile id="14">
+  <image source="tiles/15.png" width="96" height="70"/>
+ </tile>
+ <tile id="15">
+  <image source="tiles/16.png" width="96" height="70"/>
+ </tile>
+ <tile id="16">
+  <image source="tiles/17.png" width="96" height="74"/>
+ </tile>
+ <tile id="17">
+  <image source="tiles/18.png" width="96" height="96"/>
+ </tile>
+ <wangsets>
+  <wangset name="winter" type="mixed" tile="-1">
+   <wangcolor name="winter" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="3" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="4" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="5" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="7" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="8" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="9" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="11" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="12" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="13" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="14" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="15" wangid="0,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
