8d67e71627cbfa1872269d1453e9ee24b955fb06
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-12-10 19:09:01 +0200

    Fixed another button

--- src/display_data.py
+++ src/display_data.py
@@ -258,7 +258,9 @@ class FinishScreen(OnScreen):
     def go_to_main_menu(self):
         self.sound.stop()
 
-        utils.current_screen = self.level.level_menu.back
+        from main_menu import MainMenu
+
+        utils.current_screen = MainMenu(self.screen)
 
     def next_level(self):
         self.sound.stop()
